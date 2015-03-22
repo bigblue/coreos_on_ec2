961c8a0178a7ed61cfbeca7d7f048df24301ebf2
Michael Vigor <michael@bigbluedev.com>
2015-03-22 22:04:59 +0000

    Initial commit

---
+++ _common.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Detect whether output is piped or not.
+[[ -t 1 ]] && piped=0 || piped=1
+
+# Defaults
+quiet=0
+verbose=0
+args=()
+
+### Helpers
+#####################################################################
+
+out() {
+  ((quiet)) && return
+
+  local message="$@"
+  if ((piped)); then
+    message=$(echo $message | sed '
+      s/\\[0-9]\{3\}\[[0-9]\(;[0-9]\{2\}\)\?m//g;
+      s/✖/Error:/g;
+      s/✔/Success:/g;
+    ')
+  fi
+  printf '%b\n' "$message";
+}
+die() { out "$@"; exit 1; } >&2
+err() { out " \033[1;31m✖\033[0m  $@"; } >&2
+success() { out " \033[1;32m✔\033[0m  $@"; }
+
+# Verbose logging
+log() { (($verbose)) && out "$@"; }
+
+# Notify on function success
+notify() { [[ $? == 0 ]] && success "$@" || err "$@"; }
+
+# Escape a string
+escape() { echo $@ | sed 's/\//\\\//g'; }
+
+# A non-destructive exit for when the script exits naturally.
+safe_exit() {
+  trap - INT TERM EXIT
+  exit
+}
+
+function regionIndex() {
+  selectedRegion=$1
+  all_regions=('eu-west-1' 'eu-central-1' 'us-east-1' 'us-west-1' 'us-west-2' 
+               'ap-northeast-1' 'ap-southeast-1' 'ap-southeast-2' 'sa-east-1')
+  for (( i = 0; i < ${#all_regions[@]}; i++ )); do
+     if [ "${all_regions[$i]}" = "${selectedRegion}" ]; then
+         echo $i;
+     fi
+  done
+}
+
+check_aws_installed() {
+  err_msg="The aws command line client is required to use these bash scripts. 
+  Run 'pip install awscli'
+  or see https://github.com/aws/aws-cli for more information."
+  hash aws 2>/dev/null || die "$err_msg"
+}
+
+check_aws_setup() {
+  aws ec2 describe-regions > /dev/null 2> /dev/null
+  aws_out=$?
+  if [ $aws_out -ne 0 ]; then
+    die "Run 'aws configure' to setup your access details first."
+  fi
+}
+
+check_aws_cli() {
+  check_aws_installed
+  check_aws_setup
+}
---
+++ listInstances.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+#
+# List currently running instances in coreos cluster
+# Version 0.0.1
+#
+# Run ./listInstances.sh -h for usage information
+#
+#include helpers
+source "./_common.sh"
+
+### Script logic
+#####################################################################
+
+version="v0.0.1"
+
+# Print usage
+usage() {
+  echo -n "$(basename $0) [OPTION]... 
+
+List currently running instances in coreos cluster
+
+Options:
+  -g, --group     Filter by cluster security group [default: coreosgroup]
+  -r, --region    Filter by region, comma seperate multiple regions 
+                  e.g. 'us-east-1, us-west-1' [default: all regions]
+
+  -s, --skip-aws-checks    Skip checking if aws cli is installed
+  -h, --help        Display this help and exit
+      --version     Output version information and exit
+"
+}
+
+# Set a trap for cleaning up in case of errors or when script exits.
+rollback() {
+  die
+}
+
+main() {
+  if [ "$regions" == "all" ]; then
+    regions=(eu-central-1 sa-east-1 ap-northeast-1 eu-west-1 us-east-1 us-west-1 us-west-2 ap-southeast-2 ap-southeast-1)
+  else
+    regions=($(echo "$regions" | tr -d '[[:space:]]' | tr "," "\n" ))
+  fi
+
+  regionLength=${#regions[@]}
+
+  if [ $regionLength -gt 1 ]; then
+    printf "%s\n" "${regions[@]}" | xargs -n 1 -P "$regionLength" ./$0 -s "true" -g "$securitygroup" -r 
+  else
+    aws --region "$regions" ec2 describe-instances --filters Name=group-name,Values=$securitygroup,Name=instance-state-name,Values=running --output text | cut -s -f 8,16 | sed '/^$/d' | sed "s/^/$regions  /"
+  fi
+}
+
+### Boilerplate
+#####################################################################
+
+# Iterate over options breaking -ab into -a -b when needed and --foo=bar into
+# --foo bar
+optstring=h
+unset options
+while (($#)); do
+  case $1 in
+    # If option is of type -ab
+    -[!-]?*)
+      # Loop over each character starting with the second
+      for ((i=1; i < ${#1}; i++)); do
+        c=${1:i:1}
+
+        # Add current char to options
+        options+=("-$c")
+
+        # If option takes a required argument, and it's not the last char make
+        # the rest of the string its argument
+        if [[ $optstring = *"$c:"* && ${1:i+1} ]]; then
+          options+=("${1:i+1}")
+          break
+        fi
+      done
+      ;;
+    # If option is of type --foo=bar
+    --?*=*) options+=("${1%%=*}" "${1#*=}") ;;
+    # add --endopts for --
+    --) options+=(--endopts) ;;
+    # Otherwise, nothing special
+    *) options+=("$1") ;;
+  esac
+  shift
+done
+set -- "${options[@]}"
+unset options
+
+# Set our rollback function for unexpected exits.
+trap rollback INT TERM EXIT
+
+### Main loop
+#####################################################################
+
+# Read the options and set stuff
+securitygroup="coreosgroup"
+regions="all"
+skip_aws_checks=false
+
+while [[ $1 = -?* ]]; do
+  case $1 in
+    -h|--help) usage >&2; safe_exit ;;
+    --version) out "$(basename $0) $version"; safe_exit ;;
+    -g|--group) shift; securitygroup=$1 ;;
+    -r|--region) shift; regions=$1 ;;
+    -s|--skip-aws-checks) shift; skip_aws_checks=true ;;
+    --endopts) shift; break ;;
+  esac
+  shift
+done
+
+# Store the remaining part as arguments.
+args+=("$@")
+
+### Run it
+#####################################################################
+
+if ! [ $skip_aws_checks == true ]; then
+  check_aws_cli
+fi
+
+main
+
+safe_exit
